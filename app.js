339811e9c52c6e006dbe92581d125dc44f709ea6
HaoPhu20802 <thphu20802@gmail.com>
2024-02-21 14:20:44 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -7,8 +8,11 @@ app.use(cors());
 app.use(express.json());
 
 
+
 app.get("/", (req, res)=>{
     res.json({message : "Wecome to contact book application."});
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
